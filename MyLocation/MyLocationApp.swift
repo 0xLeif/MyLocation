be57d247153f139bcf7056deb9e72db837736886
Leif <zmeriksen@icloud.com>
2022-07-20 18:22:46 -0600

    Initial Commit

---
+++ MyLocation/MyLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLocationApp.swift
+//  MyLocation
+//
+//  Created by Leif on 7/20/22.
+//
+
+import SwiftUI
+
+@main
+struct MyLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
