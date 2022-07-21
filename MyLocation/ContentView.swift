45e1021be8080afd15e643ec1fc4531d3db3dd66
Leif <zmeriksen@icloud.com>
2022-07-20 18:47:41 -0600

    init poc

--- MyLocation/ContentView.swift
+++ MyLocation/ContentView.swift
@@ -5,17 +5,32 @@
 //  Created by Leif on 7/20/22.
 //
 
+import CacheStore
+import CoreLocation
+import LocationCacheStore
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var store: Store<LocationStoreKey, Void, Void>
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        if let location = store.get(.location, as: CLLocation.self) {
+            Text("Location: \(location)")
+                .padding()
+        } else {
+            ProgressView()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(
+            store: Store<LocationStoreKey, Void, Void>(
+                initialValues: [:],
+                actionHandler: .none,
+                dependency: ()
+            )
+        )
     }
 }
--- MyLocation/MyLocationApp.swift
+++ MyLocation/MyLocationApp.swift
@@ -5,13 +5,36 @@
 //  Created by Leif on 7/20/22.
 //
 
+import CacheStore
+import CoreLocation
+import LocationCacheStore
 import SwiftUI
+
+var count: CGFloat {
+    .random(in: 0 ... 100)
+}
 
 @main
 struct MyLocationApp: App {
+    @ObservedObject var locationStore = LocationStore(
+        dependency: LocationStoreDependency(
+            updateLocation: {
+                CLLocation(latitude: count, longitude: count)
+            }
+        )
+    ).debug
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(
+                store: locationStore.actionlessScope(
+                    keyTransformation: (
+                        from: { $0 },
+                        to: { $0 }
+                    ),
+                    dependencyTransformation: { _ in () }
+                )
+            )
         }
     }
 }
